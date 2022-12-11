36d2d02e216e197371355a452225abd813122318
cmcoke <minoru.cm@gmail.com>
2022-12-11 15:08:30 -0500

    created a custom 404 page.

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,18 @@
+import Head from 'next/head'
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum aliquam veritatis accusantium fuga explicabo, earum quisquam a maxime autem reprehenderit officia voluptate. Sunt maxime facilis nemo, vitae commodi dolor deleniti?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum aliquam veritatis accusantium fuga explicabo, earum quisquam a maxime autem reprehenderit officia voluptate. Sunt maxime facilis nemo, vitae commodi dolor deleniti?</p>
-    </div>
+    <>
+      <Head>
+        <title>User List | About</title>
+        <meta name="keywords" content="about" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum aliquam veritatis accusantium fuga explicabo, earum quisquam a maxime autem reprehenderit officia voluptate. Sunt maxime facilis nemo, vitae commodi dolor deleniti?</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum aliquam veritatis accusantium fuga explicabo, earum quisquam a maxime autem reprehenderit officia voluptate. Sunt maxime facilis nemo, vitae commodi dolor deleniti?</p>
+      </div>
+    </>
   )
 }
 export default About
--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,20 @@ import styles from '@/styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est libero perferendis, sapiente minus id voluptas earum voluptate assumenda neque hic? Porro maxime commodi ratione minus cum perspiciatis quas ipsum eos.</p>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est libero perferendis, sapiente minus id voluptas earum voluptate assumenda neque hic? Porro maxime commodi ratione minus cum perspiciatis quas ipsum eos.</p>
-      <Link href='/users' legacyBehavior>
-        <a className={styles.btn}>User Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>User List | Home</title>
+        <meta name="keywords" content="users" />
+        <meta name="description" content="A User list created with Next.js" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est libero perferendis, sapiente minus id voluptas earum voluptate assumenda neque hic? Porro maxime commodi ratione minus cum perspiciatis quas ipsum eos.</p>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est libero perferendis, sapiente minus id voluptas earum voluptate assumenda neque hic? Porro maxime commodi ratione minus cum perspiciatis quas ipsum eos.</p>
+        <Link href='/users' legacyBehavior>
+          <a className={styles.btn}>User Listing</a>
+        </Link>
+      </div>
+    </>
   )
 }
